b87d950142d2dea4636de430efe1f4e6c953b104
kikusu-regend <58199862+kikusu-regend@users.noreply.github.com>
2019-12-19 20:16:31 +0900

    Automatic updating　実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,36 +1,61 @@
 $(function() {
-      function buildHTML(message){
-        if ( message.image ) {
+
+  var reloadMessages = function() {
+
+    last_message_id = $('.main_chat__contents__tweets:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.main_chat__contents').append(insertHTML);
+      $('.main_chat__contents').animate({ scrollTop: $('.main_chat__contents')[0].scrollHeight});
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+
+
+      var buildHTML = function(message){
+        const BUILD_HTML = 
+        `<div class="main_chat__contents__tweets" data-message-id=` + message.id + `>` +
+          `<div class="main_chat__contents__tweets__name">` +
+            message.user_name +
+          `</div>` +   
+          `<div class="main_chat__contents__tweets__date">` +
+            message.created_at +
+          `</div>` + 
+        `</div>`   
+        if (message.content && message.image ) {
           var html = 
-            `<div class="main_chat__contents__tweets" data-message-id=${message.id}>
-              <div class="main_chat__contents__tweets__name">
-                ${message.user_name}
-              </div>   
-              <div class="main_chat__contents__tweets__date">
-                ${message.date}
-              </div> 
-            </div>   
-            <div class="main_chat__contents__massage">
-              ${message.content}
-            </div>  
-            <img src=${message.image} >` 
-          return html;
-        } else { 
+            BUILD_HTML  + 
+            `<div class="main_chat__contents__massage">` +
+              message.content +
+            `</div>` +
+            `<img src="` + message.image + `" >` 
+        } else if (message.content) { 
           var html =
-            `<div class="main_chat__contents__tweets" data-message-id=${message.id}>
-              <div class="main_chat__contents__tweets__name">
-                ${message.user_name}
-              </div>   
-              <div class="main_chat__contents__tweets__date">
-                ${message.date}
-              </div> 
-            </div>   
-            <div class="main_chat__contents__massage">
-              ${message.content}
-            </div>` 
-          return html;
+            BUILD_HTML +
+            `<div class="main_chat__contents__massage">` +
+              message.content +
+            `</div>` 
+        } else if (message.image) {
+          var html =
+            BUILD_HTML +
+            `<img src="` + message.image + `" >` 
         };
-      }
+        return html;
+      };
   $('#new_message').on('submit', function(e){
     e.preventDefault(); 
     var formData = new FormData(this);
@@ -56,3 +81,4 @@ $(function() {
     return false;
   });
 });
+
